b6f390cd504612d45355d695796ec61fecceaae2
sharkkato103 <michael.murphy@students.olin.edu>
2011-02-06 12:47:47 -0500

    Ruby Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMashUpLive::Application.config.secret_token = '39c07b28449e83dd6b547828a60f9ee1798911f849596b15ea3e8a2fe6b4c3f997a6e7183a1a78626a986546b882ba84e0fc0d730d48110b6e9e3306f1ef01ab'
