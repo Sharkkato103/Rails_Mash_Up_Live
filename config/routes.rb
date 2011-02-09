92aa727a75fc4161f53f2a2a9c2a1844786a4112
nmonje <nicholas.monje@gmail.com>
2011-02-08 23:31:17 -0500

    WHY

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,8 @@ RailsMashUpLive::Application.routes.draw do
 	match "/questions/by_course_id/:course_id" => "questions#by_course_id"
 	match "/responses/by_question_id/:question_id" => "responses#by_question_id"
 	
-	match "mobile/questions" => "questions#mobile_create", :via => :post
-	
-	match "mobile/responses" => "responses#mobile_create", :via => :post
+	match "mobile/questions" => "questions#mobile_create"	
+	match "mobile/responses" => "responses#mobile_create"
 
 	root :to => "Courses#index"
 	
